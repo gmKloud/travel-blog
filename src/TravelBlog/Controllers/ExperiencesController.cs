8a4a3e804e7824df862075258d92e8dcb3360346
grinil khanna and khanna grinil <grinil+khanna@>
2017-04-19 11:48:00 -0700

    Try migrations and delete. Start index views and controller views. Work on crud.

--- src/TravelBlog/Controllers/ExperiencesController.cs
+++ src/TravelBlog/Controllers/ExperiencesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TravelBlog.Models;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,10 +11,14 @@ namespace TravelBlog.Controllers
 {
     public class ExperiencesController : Controller
     {
+
+        //DB connection context setup
+        private TravelBlogContext db = new TravelBlogContext();
+
         // GET: /<controller>/
         public IActionResult Index()
         {
-            return View();
+            return View(db.Experiences.ToList());
         }
     }
 }
--- src/TravelBlog/Models/TravelBlogContext.cs
+++ src/TravelBlog/Models/TravelBlogContext.cs
@@ -1,15 +1,38 @@
-﻿
+﻿using System;
 using Microsoft.EntityFrameworkCore;
 
 namespace TravelBlog.Models
 {
     public class TravelBlogContext : DbContext
     {
+
+        public TravelBlogContext()
+        {
+
+        }
+
         public virtual DbSet<Location> Locations { get; set; }
+        public DbSet<People> Peoples { get; set; }
+        public DbSet<Experience> Experiences { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder options)
         {
-            options>UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=TravelBlog;integrated security=True");
+            options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=TravelBlog;integrated security=True");
+        }
+
+        private DbContextOptionsBuilder UseSqlServer(string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TravelBlogContext(DbContextOptions<TravelBlogContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
         }
     }
 }
